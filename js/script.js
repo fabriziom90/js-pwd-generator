43e3c15b40619330855e73c186feb23c3d9f9957
fabriziom90 <fabriziomastrobattista@gmail.com>
2024-06-25 09:44:44 +0200

    [UPD] Added prompt for last name

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 // PERMETTO L'INSERIMENTO DEL NOME DA PARTE DELL'UTENTE
 let first_name = prompt('Inserisci il tuo nome');
 console.log(first_name);
+
+// PERMETTO L'INSERIMENTO DEL COGNOME DA PARTE DELL'UTENTE
+let last_name = prompt('Inserisci il cognome');
+console.log(last_name);
+
+
